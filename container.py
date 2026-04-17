0540dd245442ab9b19df6b144ed538c9f76f379e
Gabriel <ramirezenrique2061@gmail.com>
2026-04-16 22:57:38 -0600

    Create Manager class for Mini Market application
    Initialize the main application window for Mini Market.

---
+++ container.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+import sys
+import os
+
+class Manager(Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Mini Market V1.0")
+        self.geometry("1100x650+120+20")
+        self.resizable(False, False)
+
+        container = Frame(self)
+        container.pack(side=TOP, fill=BOTH, expand=True)
+        container.configure(bg="#C6D9E3")
